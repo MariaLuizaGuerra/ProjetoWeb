05a531910a0f652d60cf62712cfe1bef74f249ec
Jair Ferronato <ferronatto11@hotmail.com>
2025-10-23 10:26:51 -0300

    Ajustes usuarios

---
+++ usuarios/inserirUsuario.php
@@ -0,0 +1,27 @@
+<?php
+    require_once __DIR__ . '/../src/conexao-bd.php';
+    require_once __DIR__ . '/../src/Repositorio/UsuarioRepositorio.php';
+    require_once __DIR__ . '/../src/Modelo/Usuario.php';
+
+    $nome = 'Fulano de Tal';
+    $perfil = 'user';
+    $email = 'fulano@exemplo.com';
+    $senha = '1234';
+  
+
+    $repo = new UsuarioRepositorio($pdo);
+    //Verificar se o usuario existe
+    if($repo->buscarPorEmail($email))
+    {
+        echo "Usuario já existe! {$email}\n";
+        exit;
+    }
+
+    $repo->salvar(new Usuario(0, $nome, $perfil, $email, $senha));
+    
+
+    echo "Usuário inserido: {$email}\n";
+
+
+
+?>
