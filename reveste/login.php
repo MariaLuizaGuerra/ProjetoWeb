718a4cc89435ff78f79bf46f4a168fda4481a13b
MariaLuizaGuerra <marialuizaguerrapedroso@gmail.com>
2025-11-18 12:25:34 -0300

    Tentando 18/11

--- reveste/login.php
+++ reveste/login.php
@@ -1,20 +1,29 @@
 <?php
 session_start();
-
-$usuarioLogado = $_SESSION['email'] ?? null;
+$usuarioLogado = $_SESSION['usuario'] ?? null;
 $erro = $_GET['erro'] ?? '';
+
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-   
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/admin.css">
+    <link rel="stylesheet" href="css/form.css">
     <link rel="stylesheet" href="css/login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="icon" href="img/reVeste_Logo.jpg" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;900&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600;700&display=swap" rel="stylesheet">
 
     <title>reVeste - Login</title>
 </head>
@@ -54,6 +63,8 @@ $erro = $_GET['erro'] ?? '';
                     <input type="password" id="senha" name="senha" placeholder="Digite a sua senha" required>
 
                     <input type="submit" class="botao-entrar" value="Entrar">
+                    <input type="submit" class="botao-cadastrar" value="Cadastrar">
+
                 </form>
             </section>
 
